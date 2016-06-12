39939a86e994b033b13680adcaaa1e01c4ccc4b8
kapilsharma <kapil@kapilsharma.info>
2016-06-12 12:39:52 +0530

    Edited simple example

--- examples/simpleexample.php
+++ examples/simpleexample.php
@@ -5,8 +5,8 @@ use phpreboot\util\IntegerArray as A;
 
 $array = new A([5,3,-8,4]);
 
-echo $array->sort()->toString() . PHP_EOL;
-echo $array->sort(\phpreboot\util\IntegerArray::ORDER_DESCENDING)->toString() . PHP_EOL;
-echo "Sum is " . $array->sum() . PHP_EOL;
-echo "Product is " . $array->product() . PHP_EOL;
-echo "Absolute: " . $array->absolute()->toString() . PHP_EOL;
+echo $array->sort()->toString() . PHP_EOL;                      // -8, 3, 4, 5
+echo $array->sort(A::ORDER_DESCENDING)->toString() . PHP_EOL;   // 5,4,3,-8
+echo "Sum is " . $array->sum() . PHP_EOL;                       //Sum is 4
+echo "Product is " . $array->product() . PHP_EOL;               // Product is -480
+echo "Absolute: " . $array->absolute()->toString() . PHP_EOL;   // 5,4,3,8
